1255af8b4de707a1aa3c51d5aa6d348311062028
Adolfo González Hernández <adolfogonzal@gmail.com>
2025-03-02 20:37:02 +0100

    actualizaciones_02_03_2025
    Mejoras en setup.py y pyproject.toml

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="sqliteplus",
+    name="sqliteplus-enhanced",
     version="1.0.0",
-    author="Tu Nombre",
-    author_email="tuemail@example.com",
+    author="Adolfo González Hernández",
+    author_email="adolfogonzal@gmail.com",
     description="SQLite mejorado con cifrado SQLCipher y caché en Redis",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/Alphonsus411/sqliteplus",
+    url="https://github.com/Alphonsus411/sqliteplus-enhanced",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -23,7 +23,7 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "sqliteplus=sqliteplus.cli:cli"
+            "sqliteplus-enhanced=sqliteplus.cli:cli"
         ]
     },
     classifiers=[
